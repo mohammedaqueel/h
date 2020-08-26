036554005a744150a0a7f08fcb9e00975594df24
MD AQUEEL S <31254664+mohammedaqueel@users.noreply.github.com>
2020-08-26 14:50:18 +0530

    Add files via upload

---
+++ application/script.js
@@ -0,0 +1,24 @@
+var date = new Date();
+            
+var startDate = new Date("Aug 26, 2020 07:30:00");
+            
+var endDate = new Date("Nov 24, 2020 07:30:00");
+
+var totalDays = endDate.getTime() - startDate.getTime();
+totalDays = Math.floor( totalDays / (1000 * 60 * 60 * 24));
+
+var daysPassed = date.getTime() - startDate.getTime();
+daysPassed = Math.floor( daysPassed / (1000 * 60 * 60 * 24));
+
+var daysLeft = endDate.getTime() - date.getTime();
+daysLeft = Math.floor( daysLeft / (1000 * 60 * 60 * 24) + 1);
+
+var percentage = Math.floor((daysPassed / totalDays) * 100);
+
+document.querySelector(".percentage").innerHTML = percentage+"%";
+document.querySelector(".passed-days").innerHTML = daysPassed+" Days Passed of "+totalDays+" Days";
+document.querySelector(".left-days").innerHTML = daysLeft+" Days Left";
+document.querySelector(".bar").style.width = percentage+"%";
+document.querySelector(".today").innerHTML = date;
+document.querySelector(".startDate").innerHTML = startDate;
+document.querySelector(".endDate").innerHTML = endDate;
